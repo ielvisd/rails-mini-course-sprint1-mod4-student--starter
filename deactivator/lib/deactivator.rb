678c77d80d4b88aafa90103f4e016bff5814b76b
Elvis Ibarra <ielvisd@gmail.com>
2020-02-01 17:10:06 -0800

    New gem

---
+++ deactivator/lib/deactivator.rb
@@ -0,0 +1,6 @@
+require "deactivator/version"
+
+module Deactivator
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ deactivator/lib/deactivator/version.rb
@@ -0,0 +1,3 @@
+module Deactivator
+  VERSION = "0.1.0"
+end
